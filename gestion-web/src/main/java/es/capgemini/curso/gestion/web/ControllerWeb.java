b894bf4a8600d39b576df8b5524f77db3a057088
Álvaro Maestre <alvaro.maestre-santa@capgemini.com>
2021-02-02 09:08:42 +0100

    Correcto muestreo de las categorías en la páginas web con sus respectivos enlaces creados

--- gestion-web/src/main/java/es/capgemini/curso/gestion/web/ControllerWeb.java
+++ gestion-web/src/main/java/es/capgemini/curso/gestion/web/ControllerWeb.java
@@ -1,30 +1,48 @@
 package es.capgemini.curso.gestion.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import es.capgemini.curso.gestion.model.CategoriaDAO;
+import es.capgemini.curso.gestion.model.ProductoDAO;
+
+
 
 @Controller
 public class ControllerWeb {
 
-	/*@Autowired
-	private CategoriaDAO categoria;
-	
 	@Autowired
-	private ProductoDAO producto;*/
-	
-	@GetMapping(path = {"/"})
+	private CategoriaDAO categoria;
+
+	@Autowired
+	private ProductoDAO producto;
+
+	@GetMapping(path = { "/", "/categoria" })
 	public ModelAndView index() {
-		return new ModelAndView("index");
+		// , categoria.findAll()
+		return new ModelAndView("categoria", "categoria", categoria.findAll());
 	}
-	
-	/*@PostMapping("/categoria")
-	public ModelAndView procesarCategoria(@RequestParam("idCategoria") int id) {
-		return new ModelAndView("list-productos", "categoria", categoria.findById(id));
+
+	@GetMapping(path = { "/categoria/{idCategoria}" })
+	public ModelAndView detallesCategoria(@PathVariable("idCategoria") int idCategoria) {
+		ModelAndView mv = new ModelAndView();
+
+		mv.addObject("categoria", categoria.findById(idCategoria).get().getProductos());
+		mv.setViewName("categoria");
+
+		return mv;
 	}
-	
-	@GetMapping("/productos")
-	public ModelAndView mostrarProductos(@RequestParam("idCategoria") int id) {
-		return new ModelAndView("list-productos", "producto", producto.findById(id).get());
-	}*/
-} 
+
+	/*
+	 * @PostMapping("/categoria") public ModelAndView
+	 * procesarCategoria(@RequestParam("idCategoria") int id) { return new
+	 * ModelAndView("list-productos", "categoria", categoria.findById(id)); }
+	 * deducir conclusiones y problemas
+	 * @GetMapping("/productos") public ModelAndView
+	 * mostrarProductos(@RequestParam("idCategoria") int id) { return new
+	 * ModelAndView("list-productos", "producto", producto.findById(id).get()); }
+	 */
+}
--- gestion-web/src/main/java/es/capgemini/curso/gestion/web/config/ComponentesWeb.java
+++ gestion-web/src/main/java/es/capgemini/curso/gestion/web/config/ComponentesWeb.java
@@ -6,19 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"es.capgemini.curso.gestion.web"})
+@ComponentScan(basePackages = { "es.capgemini.curso.gestion.web" })
 public class ComponentesWeb {
 
 	@Bean
 	public ViewResolver configurarViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		
+
+		// Para usar instrucciones JSTL
+		resolver.setViewClass(JstlView.class);
 		resolver.setPrefix("WEB-INF/vistas/");
 		resolver.setSuffix(".jsp");
-		
+
 		return resolver;
 	}
 }
